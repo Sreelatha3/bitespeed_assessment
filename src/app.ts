01d94c2e4b322c2c58f612cf152a7e39db598a59
Sreelatha3 <gounipallesreelatha@gmail.com>
2025-07-02 05:51:58 +0530

    changed /get in render

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,19 @@ import express, { Request, Response } from 'express';
 import { identifyContact } from './identify';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/identify', (req, res) => {
+  res.send('Please POST to /identify with JSON: { email, phoneNumber }');
+});
+
 app.post('/identify', identifyContact);
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
